e002cdb66f2ee5af036cefbcaa04707ef5d3c56e
Christian Antonio <christian.antonio@gmp-systems.com>
2021-09-14 13:57:53 +0200

    test commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from "@angular/forms";
